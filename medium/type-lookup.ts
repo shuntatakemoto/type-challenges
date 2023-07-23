066bf5e96b117011ca98e19295437f0028af7b3e
Shunta Takemoto <shunta.takemoto@nex.nikkei.com>
2023-07-24 02:13:26 +0900

    Add medium problems

---
+++ medium/type-lookup.ts
@@ -0,0 +1,14 @@
+interface Cat {
+  type: "cat";
+  breeds: "Abyssinian" | "Shorthair" | "Curl" | "Bengal";
+}
+
+interface Dog {
+  type: "dog";
+  breeds: "Hound" | "Brittany" | "Bulldog" | "Boxer";
+  color: "brown" | "white" | "black";
+}
+
+type LookUp<U, T> = U extends { type: T } ? U : never;
+
+type MyDog = LookUp<Cat | Dog, "dog">; // expected to be `Dog`
