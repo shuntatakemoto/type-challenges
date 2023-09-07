160b7f768da861ae737b1724c4803dbf0ab65ff1
Shunta Takemoto <shunta.takemoto@nex.nikkei.com>
2023-09-07 14:07:48 +0900

    fix

--- medium/append-to-object.ts
+++ medium/append-to-object.ts
@@ -1,5 +1,5 @@
 type AppendToObject<T extends object, U extends string, V> = {
   [k in keyof T | U]: k extends keyof T ? T[k] : V;
 };
-type Test = { id: "1" };
-type Result4 = AppendToObject<Test, "value", 2>; // expected to be {id: "1", value: 2}
+type Test2 = { id: "1" };
+type Result4 = AppendToObject<Test2, "value", 2>; // expected to be {id: "1", value: 2}
